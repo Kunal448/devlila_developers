e9abb77f50a310c1bdf347633ecafe7a60e26673
Kunal Pandit <119098582+Kunal448@users.noreply.github.com>
2025-11-12 15:23:12 +0530

    main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 from streamlit_option_menu import option_menu
 
 # Import your pages here
-import home, add_site, sites, jcb_work, add_ledger, ledger
+import home, add_site, sites, jcb_work, add_ledger, ledger, vehicle_expenses
 
 st.set_page_config(page_title="Devlila Developers", page_icon="🏭", initial_sidebar_state="collapsed")
 
@@ -10,7 +10,7 @@ st.set_page_config(page_title="Devlila Developers", page_icon="🏭", initial_si
 
 with st.sidebar:
     selected = option_menu(
-        "Main Menu", ["Home", "Add Site", "Manage Sites", "Manage JCB", "Add Ledger", "Manage Ledgers"],
+        "Main Menu", ["Home", "Add Site", "Manage Sites", "Manage JCB", "Add Ledger", "Manage Ledgers", "Vehicle Expenses"],
         icons=[],
         menu_icon="cast", default_index=0,
         styles={
@@ -29,5 +29,8 @@ elif selected == "Add Ledger":
     add_ledger.page4()
 elif selected == "Manage Ledgers":
     ledger.page5()
+elif selected == "Vehicle Expenses":
+    vehicle_expenses.page6()
 else:
     st.warning("Select Page Above")
+
